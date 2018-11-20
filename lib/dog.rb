edfa79e114fefd366842dec84a7342ca1b9e23b8
Claudia Fox <claudiafox95@icloud.com>
2018-11-20 17:47:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,16 @@
-class Dog 
-  def name(dogs name)
-    @this_dogs_name = dogs_name 
+class Dog
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
-  def name 
+ 
+  def name
     @this_dogs_name
   end
 end
-
-lassie = Dog.new 
+ 
+lassie = Dog.new
 lassie.name = "Lassie"
-
+ 
 puts lassie.name
+ 
